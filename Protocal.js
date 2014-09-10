3c672040db1982fc5f23a1d13acd60adf33d9757
Jacob Bondley <hasharahha@gmail.com>
2014-09-10 06:44:07 -0400

    Small login system

---
+++ Protocal.js
@@ -0,0 +1,34 @@
+//Made by Jacob Bondley
+
+function Home(){
+window.location ='http://jbcompanyrules.blogspot.com/index.html'
+}
+
+function Auth(){
+var p;
+var u;
+var text;
+
+p = document.getElementById("password").value
+u = document.getElementById("username").value
+
+if(u=="JBteam"&&p=="JAB"){
+window.location ='http://jbcompanyrules.blogspot.com/p/jab.html';
+window.alert("logging in")
+} else{
+window.alert("incorect password")
+}
+if(u=="HASHARAHHA14"&&p==="017964"){
+window.location ='http:///jbcompanyrules.blogspot.com/p/jab.html';
+window.alert(" Admin sign-in Loading")
+}
+}
+
+
+
+
+
+
+
+
+
