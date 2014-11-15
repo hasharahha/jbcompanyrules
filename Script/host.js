7935909298873f58069a0c184a8fd415aba02521
Jacob Bondley <hasharahha@gmail.com>
2014-11-15 11:59:17 -0500

    Create host.js

---
+++ Script/host.js
@@ -0,0 +1,6 @@
+
+function Signup(){
+var member = prompt("what is your name", "name Here");
+alert("Okay to Host mods follow these steps")
+document.getElementById("instructions").style.display='block';
+}
