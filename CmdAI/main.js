1e3629b02d4b58f5d773bac8f0ebd6e02a1d60e2
Jacob Bondley <hasharahha@gmail.com>
2014-11-22 16:26:10 -0500

    Update main.js

--- CmdAI/main.js
+++ CmdAI/main.js
@@ -1,1 +1,10 @@
 //basic command set-up
+var Cache =[];
+var command;
+var Cmd;
+function ProcCmd(){
+  command = document.getElementById("CommandPrompt").value;
+  Cmd = command.split(" ");
+  //basic required Commands
+  
+}
