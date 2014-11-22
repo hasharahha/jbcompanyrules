34401f319789c53a3c55fb2d076b7a57f42bcb02
Jacob Bondley <hasharahha@gmail.com>
2014-11-22 17:49:41 -0500

    Update main.js

--- CmdAI/main.js
+++ CmdAI/main.js
@@ -2,9 +2,18 @@
 var Cache =[];
 var command;
 var Cmd;
+var log = function(msg){
+  confirm("Console: \n"+msg);
+};
 function ProcCmd(){
   command = document.getElementById("CommandPrompt").value;
   Cmd = command.split(" ");
   //basic required Commands
-  
+  if(Cmd[0]=="/help"){
+    log("Help list 1 of 1 \n /calculate")
+  }
+  else if(Cmd[0]=="/calculate"){
+    var answer = eval(Cmd[1]);
+    log("Answer: \n"+answer);
+  }
 }
