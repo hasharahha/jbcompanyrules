1b56d42392e15bcb21d2b4e343ce4bfc4bf6286d
Jacob Bondley <hasharahha@gmail.com>
2014-11-22 19:52:24 -0500

    Update main.js

--- CmdAI/main.js
+++ CmdAI/main.js
@@ -1,5 +1,6 @@
 //basic command set-up
 var Cache =[];
+var OPS =["Jacob", "Devon"]
 var command;
 var Cmd;
 var log = function(msg){
@@ -10,10 +11,14 @@ function ProcCmd(){
   Cmd = command.split(" ");
   //basic required Commands
   if(Cmd[0]=="/help"){
-    log("Help list 1 of 1 \n /calculate")
+    log("Help list 1 of 1 \n /calculate \n /op")
   }
   else if(Cmd[0]=="/calculate"){
     var answer = eval(Cmd[1]);
     log("Answer: \n"+answer);
+  }
+  else if(Cmd[0]=="/op"){
+    log(Cmd[1]+" Has Been switched to op")
+    OPS[3] =Cmd[1];
   }
 }
