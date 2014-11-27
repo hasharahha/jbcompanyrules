71afd34e086dbb670d3a0d5599db58cc48f3e1d9
Jacob Bondley <hasharahha@gmail.com>
2014-11-26 20:50:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -87,7 +87,16 @@ else if(energy=="3"){
   document.getElementById("Energy").innerHTML='';
 }
 else if(energy=="2"){
-  
+  document.getElementById("Energy").innerHTML='';
+}
+else if(energy=="1"){
+  document.getElementById("Energy").innerHTML='';
+}
+else if(energy=="0"){
+  document.getElementById("Energy").innerHTML='';
+}
+else if(energy=="10"){
+  document.getElementById("Energy").innerHTML='';
 }
 
 }
