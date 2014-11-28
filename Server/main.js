8c0db99d9e7668ce8cd9c671078e2bfc1610602a
Jacob Bondley <hasharahha@gmail.com>
2014-11-27 21:25:24 -0500

    Update main.js

--- Server/main.js
+++ Server/main.js
@@ -1,5 +1,12 @@
-var players =["Etho", "Docm77", "sethbling", "captainsparklez", "ZexyZek", "Herobrine", "HASHARAHHA14", "Joebuz", "daNoob1324", "HerobrineSlayer12", "SkythekidRS", "NoobsRule", "Blacksoul", "asdfsdsdsdsdsdfsdfsd", "SuperCreeper"];
+var players =["Etho", "Docm77", "sethbling", "captainsparklez", "ZexyZek", "Herobrine", "OMGitsFireFox", "Joebuz", "daNoob1324", "HerobrineSlayer12", "SkythekidRS", "NoobsRule", "Blacksoul", "asdfsdsdsdsdsdfsdfsd", "SuperCreeper"];
 var Selected_player =[];
+var Online =[];
+var split =", ";
 Selected_player[0] = Math.floor(Math.random()*11);
 Selected_player[1] = Math.floor(Math.random()*11);
-var Online = players[Selected_player[0]];
+Selected_player[2] = Math.floor(Math.random()*11);
+Online[0] = players[Selected_player[0]];
+Online[1] = players[Selected_player[1]];
+Online[2] = players[Selected_player[2]];
+
+confirm("Server: IP:************ Port: **** \n Online Players: \n"+" "+Online[0]+split+Online[1]+split+Online[2]);
