613a31d33b453e46638c87ce87ac450fc608f8a7
Jacob Bondley <hasharahha@gmail.com>
2014-11-27 21:06:37 -0500

    Create main.js

---
+++ Server/main.js
@@ -0,0 +1,3 @@
+var players =["Etho", "Docm77", "sethbling", "captainsparklez", "ZexyZek", "Herobrine", "HASHARAHHA14"];
+var Selected_player = Math.floor(Math.random()*7);
+var Online = players[Selected_player];
