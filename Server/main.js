332924162b46f003eb8fd012cb6cc12ccf4c4184
Jacob Bondley <hasharahha@gmail.com>
2014-11-27 21:15:41 -0500

    Update main.js

--- Server/main.js
+++ Server/main.js
@@ -1,3 +1,3 @@
-var players =["Etho", "Docm77", "sethbling", "captainsparklez", "ZexyZek", "Herobrine", "HASHARAHHA14"];
+var players =["Etho", "Docm77", "sethbling", "captainsparklez", "ZexyZek", "Herobrine", "HASHARAHHA14", "Joebuz", "daNoob1324", "HerobrineSlayer12", "SkythekidRS", ];
 var Selected_player = Math.floor(Math.random()*7);
 var Online = players[Selected_player];
