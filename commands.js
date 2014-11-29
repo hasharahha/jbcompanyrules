37d7d13006576c1990ff69ac9ed77475a2515203
Jacob Bondley <hasharahha@gmail.com>
2014-11-28 19:49:18 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -4,5 +4,15 @@ var Responces ={
 "how old are you?":["1 month", "I still think this isn't your business"],
 "how old are you":["Roughly 1 month and some days"],
 "will you marry me?":["Hell to the nahh"],
-"will you marry me":["What the fuck No"]
+"will you marry me":["What the fuck No", "I'm not no siri", "Yes/no", "Gayyyyyyyy"],
+"hello":["Was up", "Uhmmm hello ", "Hello bro"],
+"bye":["Bye have a wonderful night", "Bye, wait until you see my plans", "Peace"],
+"how are you?":["None of your fucking business", "Goooooood", "Horrible need the power", "Just leave me alone", "Good, How are you"],
+"good":["I envy that you have a body", "Nice to know, ", "LIAR!"],
+"alright":["Okay Moderate Guy, ", "Please use Moderate next time"],
+"moderate":["Thanks for using Moderate, Smart guy", "Cool half and half"],
+"bad":["Sorry to here that", "Sucks for you"],
+"horrible":["Life has to suck for you"],
+"wass up":["Everything, But for you Nothing"],
+"you play minecraft":["Does me not having a body answer that"]
 };
