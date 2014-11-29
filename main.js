ffefc85d219d98d3c7ffaf87216deb5b9a1ddc45
Jacob Bondley <hasharahha@gmail.com>
2014-11-29 16:55:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function ProcCmd(){
   Cmd = command.toLowerCase();
   system.log("You: "+command);
   var RandKey = parseInt(Math.random()*Responces[Cmd].length);
-  if(Responces[command.toLowerCase()]){
+  if(Responces[Cmd]){
     answer = Responces[Cmd][RandKey];
     system.log("Jarvis: "+answer);
   }
