ee1ef8b493bf320f445be3e02ffbf36fbe74e097
Jacob Bondley <hasharahha@gmail.com>
2014-11-28 19:07:13 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,4 +1,8 @@
 var Responces ={
-"who are you":"Jarvis V100",
-"how old are you":"1 month"
+"who are you?":"Jarvis V100",
+"who are you":"Jarvis",
+"how old are you?":"1 month",
+"how old are you":"Roughly 1 month and some days",
+"will you marry me?":"Hell to the nahh",
+"will you marry me":"What the fuck No",
 };
