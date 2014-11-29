b5d76859d8cac0285dbafb0cb650b483eda5c875
Jacob Bondley <hasharahha@gmail.com>
2014-11-28 19:34:56 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -4,5 +4,5 @@ var Responces ={
 "how old are you?":["1 month", "I still think this isn't your business"],
 "how old are you":["Roughly 1 month and some days"],
 "will you marry me?":["Hell to the nahh"],
-"will you marry me":["What the fuck No"],
+"will you marry me":["What the fuck No"]
 };
