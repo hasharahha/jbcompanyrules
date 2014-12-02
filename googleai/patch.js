9f40f7233588d074002d633fc1a5e5ca7d1555b7
Jacob Bondley <hasharahha@gmail.com>
2014-12-02 15:44:20 -0500

    Update patch.js

--- googleai/patch.js
+++ googleai/patch.js
@@ -1,4 +1,4 @@
-alert("Google AI patched V2.0");
+alert("Google AI patched V3.0");
 var crawler = document.createElement("input");
 crawler.innerHTML="Jacob has edited Google Chrome sucessfuly";
 document.body.appendChild(crawler);
