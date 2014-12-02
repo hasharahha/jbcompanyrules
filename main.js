21320981af19eaa11b4f3af1266391f1fe7d2776
Jacob Bondley <hasharahha@gmail.com>
2014-12-01 19:35:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,9 @@ function ProcCmd(){
   var RandKey = parseInt(Math.random()*Responces[Cmd].length);
     answer = Responces[Cmd][RandKey];
   system.log("Jarvis: "+answer)
-if(typeof Cmd === "undefined"){
+if(typeof Cmd === "undefined" | typeof Cmd ==="null" | typeof Cmd === null | typeof Cmd === undefined){
   var rnd = parseInt(Math.random()*4);
+  log("e");
   system.log("Jarvis: "+Responces["undefined"][rnd])
 }
   //basic required Commands
