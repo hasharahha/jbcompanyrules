e0c1056a1a4bbabc84cfebb240ad2fed5caf93b9
Jacob Bondley <hasharahha@gmail.com>
2014-12-02 11:33:16 -0500

    Create patch.js

---
+++ googleai/patch.js
@@ -0,0 +1,6 @@
+alert("Google AI patched");
+var crawler = document.createElement("input");
+
+document.getElementsByTageName("input").style.backgroundColor="blue";
+document.getElementsByTagName("input").style.color="white";
+
